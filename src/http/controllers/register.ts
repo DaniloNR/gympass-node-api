300b14422aed417ab07fc7d98baa4419a86cd1f6
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-24 16:52:20 -0300

    feat: move out of the controller the use case for user creation to a separate file

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,7 +1,6 @@
 import { z } from "zod";
-import { prisma } from "@/lib/prisma";
 import { FastifyRequest as Req, FastifyReply as Rep } from "fastify";
-import { hash } from "bcryptjs";
+import { registerUseCase } from "@/use-cases/register";
 
 export async function register(request: Req, reply: Rep): Promise<Rep> {
   const registerBodySchema = z.object({
@@ -12,25 +11,15 @@ export async function register(request: Req, reply: Rep): Promise<Rep> {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
-  const password_hash = await hash(password, 6);
-
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
-      email,
-    },
-  });
-
-  if (userWithSameEmail) {
-    return reply.status(409).send();
-  }
-
-  await prisma.user.create({
-    data: {
+  try {
+    await registerUseCase({
       name,
       email,
-      password_hash,
-    },
-  });
+      password,
+    });
+  } catch (err) {
+    return reply.status(409).send();
+  }
 
   return reply.status(201).send();
 }
