3a06677e44402cc1e2378c573b4abdd2942a4554
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-24 16:43:10 -0300

    feat: add password hashing to the user creation

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,6 +1,7 @@
 import { z } from "zod";
 import { prisma } from "@/lib/prisma";
 import { FastifyRequest as Req, FastifyReply as Rep } from "fastify";
+import { hash } from "bcryptjs";
 
 export async function register(request: Req, reply: Rep): Promise<Rep> {
   const registerBodySchema = z.object({
@@ -11,11 +12,23 @@ export async function register(request: Req, reply: Rep): Promise<Rep> {
 
   const { name, email, password } = registerBodySchema.parse(request.body);
 
+  const password_hash = await hash(password, 6);
+
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (userWithSameEmail) {
+    return reply.status(409).send();
+  }
+
   await prisma.user.create({
     data: {
       name,
       email,
-      password_hash: password,
+      password_hash,
     },
   });
 
