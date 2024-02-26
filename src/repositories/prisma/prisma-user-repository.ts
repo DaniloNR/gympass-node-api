a9eb0a773a5ff026d6b18df2d382e198ce567919
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-26 15:01:41 -0300

    feat: create new use case for getting user profile

--- src/repositories/prisma/prisma-user-repository.ts
+++ src/repositories/prisma/prisma-user-repository.ts
@@ -3,6 +3,16 @@ import type { Prisma } from "@prisma/client";
 import { UsersRepository } from "../users-repository";
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: Prisma.UserCreateInput["id"]) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
       data,
--- src/repositories/users-repository.d.ts
+++ src/repositories/users-repository.d.ts
@@ -1,6 +1,7 @@
 import { Prisma, User } from "@prisma/client";
 
 export interface UsersRepository {
+  findById(id: string): Promise<User | null>;
   create(data: Prisma.UserCreateInput): Promise<User>;
   findByEmail(email: Prisma.UserCreateInput["email"]): Promise<User | null>;
 }
