2db77193e971b15a1f91fb85a837ab50e56fdabf
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-25 22:05:16 -0300

    feat: add new route to router list

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,8 @@
-import { register } from "@/http/controllers/register";
 import { FastifyInstance } from "fastify";
+import { register } from "@/http/controllers/register";
+import { authenticate } from "@/http/controllers/authenticate";
 
 export async function AppRoutes(app: FastifyInstance) {
   app.post("/users", register);
+  app.post("/session", authenticate);
 }
