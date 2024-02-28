f7cc34b138dae92c58e0920157702330e95acd7d
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-28 15:23:49 -0300

    feat: add check-in validation, repositories and tests

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -8,6 +8,12 @@ dayjs.extend(isBetween);
 export class InMemoryCheckInsRepository implements CheckInsRepository {
   public items: CheckIn[] = [];
 
+  async findById(id: string) {
+    const checkIn = this.items.find((item) => item.id === id);
+
+    return checkIn || null;
+  }
+
   async findByUserIdOnDate(userId: string, date: Date) {
     const startOfTheDay = dayjs(date).startOf("date");
     const endOfTheDay = dayjs(date).endOf("date");
@@ -50,6 +56,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     };
 
     this.items.push(checkIn);
+
+    return checkIn;
+  }
+
+  async save(checkIn: CheckIn) {
+    const checkInIndex = this.items.findIndex((item) => item.id === checkIn.id);
+
+    if (checkInIndex >= 0) {
+      this.items[checkInIndex] = checkIn;
+    }
 
     return checkIn;
   }
