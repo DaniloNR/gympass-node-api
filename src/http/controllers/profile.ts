38a8731afe5c4e411859b040e2b1dce145eab594
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-28 21:54:40 -0300

    feat: add controller for profile

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,7 +1,19 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use.case";
 import { FastifyRequest as Req, FastifyReply as Rep } from "fastify";
 
 export async function profile(request: Req, reply: Rep): Promise<Rep> {
-  await request.jwtVerify();
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  return reply.status(200).send();
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  });
+
+  return reply.status(200).send({
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+    },
+  });
 }
