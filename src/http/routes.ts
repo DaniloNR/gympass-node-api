0b3852465aa6b29a1f77a5e0dfb78e9b07d8b10a
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-02-28 21:54:24 -0300

    feat: create middleware for auth by jwt token verification

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,11 +2,12 @@ import { FastifyInstance } from "fastify";
 import { register } from "@/http/controllers/register";
 import { authenticate } from "@/http/controllers/authenticate";
 import { profile } from "@/http/controllers/profile";
+import { verifyJwt } from "./middlewares/verify-jwt";
 
 export async function AppRoutes(app: FastifyInstance) {
   app.post("/users", register);
   app.post("/session", authenticate);
 
   // auth routes
-  app.get("/me", profile);
+  app.get("/me", { onRequest: [verifyJwt] }, profile);
 }
