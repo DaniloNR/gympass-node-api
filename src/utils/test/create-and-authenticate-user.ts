0b4f8b47f51f966f4cf2dd0bee8379e84b2cb36e
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-03-02 18:55:43 -0300

    feat: create utility to create and login a new user before a test

---
+++ src/utils/test/create-and-authenticate-user.ts
@@ -0,0 +1,21 @@
+import { FastifyInstance } from "fastify";
+import request from "supertest";
+
+export async function createAndAuthenticateUser(app: FastifyInstance) {
+  await request(app.server).post("/users").send({
+    name: "John Doe",
+    email: "johndoe@example.com",
+    password: "123456",
+  });
+
+  const {
+    body: { token },
+  } = await request(app.server).post("/sessions").send({
+    email: "johndoe@example.com",
+    password: "123456",
+  });
+
+  return {
+    token,
+  };
+}
