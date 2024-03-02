6bdf9c5e1a23f5d6d90b55d4e133d8587ee06961
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-03-02 17:52:58 -0300

    feat: create e2e tests for auth and register controllers

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,33 @@
-import { test } from "vitest";
+import request from "supertest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
 
-test("ok", () => {});
+describe("Authenticate (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to authenticate", async () => {
+    const jwtRegex = /^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_=]+$/;
+
+    await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    const { status, body } = await request(app.server).post("/session").send({
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(status).toEqual(200);
+    expect(body).toEqual({
+      token: expect.stringMatching(jwtRegex),
+    });
+  });
+});
--- src/http/controllers/register.spec.ts
+++ src/http/controllers/register.spec.ts
@@ -1,3 +1,23 @@
-import { test } from "vitest";
+import { app } from "@/app";
+import { afterAll, beforeAll, describe, expect, it } from "vitest";
+import request from "supertest";
 
-test("ok", () => {});
+describe("Register (e2e)", () => {
+  beforeAll(async () => {
+    await app.ready();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  it("should be able to register", async () => {
+    const { statusCode } = await request(app.server).post("/users").send({
+      name: "John Doe",
+      email: "johndoe@example.com",
+      password: "123456",
+    });
+
+    expect(statusCode).toEqual(201);
+  });
+});
