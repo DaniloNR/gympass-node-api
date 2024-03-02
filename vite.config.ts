2aff12f3dfa02e8e5e0c31ac02cea159a0a2b0a4
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-03-02 16:03:36 -0300

    feat: add config for test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
+  test: {
+    environmentMatchGlobs: [
+      ["src/http/controllers/**", "./vitest-environments/prisma.ts"],
+    ],
+    dir: "src",
+  },
 });
