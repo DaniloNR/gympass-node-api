2662ebf8947755d476b31709931be416249063f6
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-03-02 16:20:37 -0300

    feat: add configuration for vitest enviroment prisma

--- vitest-environments/prisma.ts
+++ vitest-environments/prisma.ts
@@ -1,14 +1,38 @@
+import { randomUUID } from "node:crypto";
+import { execSync } from "node:child_process";
 import { Environment } from "vitest";
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+
+const prisma = new PrismaClient();
+
+function generateNewRandomDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable.");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   transformMode: "ssr",
   async setup() {
-    console.log("Setup");
+    const schema = randomUUID();
+    process.env.DATABASE_URL = generateNewRandomDatabaseURL(schema);
+
+    execSync("npx prisma migrate deploy");
 
     return {
       async teardown() {
-        console.log("Teardown");
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
+
+        await prisma.$disconnect();
       },
     };
   },
