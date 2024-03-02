890cf11ac3cec55880fe7bdafa0e45ab04f4c012
Danilo Nogueira <danilo.nogueira1992@gmail.com>
2024-03-02 18:24:52 -0300

    feat: create more gyms controllers and update routes

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,6 +1,14 @@
 import { FastifyInstance } from "fastify";
 import { verifyJwt } from "@/http/middlewares/verify-jwt";
+import { search } from "@/http/controllers/gyms/search";
+import { nearby } from "@/http/controllers/gyms/nearby";
+import { create } from "@/http/controllers/gyms/create";
 
 export async function gymsRoutes(app: FastifyInstance) {
   app.addHook("onRequest", verifyJwt);
+
+  app.get("/gyms/search", search);
+  app.get("/gyms/nearby", nearby);
+
+  app.post("/gyms", create);
 }
